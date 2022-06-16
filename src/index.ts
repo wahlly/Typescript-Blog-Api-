26d80ac5b9f0be24a3e787c4e2443de414bc1cb6
wahlly <olaifaolawaleh@gmail.com>
2022-06-16 09:26:49 +0100

    create post

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import 'dotenv/config';
 import 'module-alias/register';
 import validateEnv from '@/utils/validateEnv';
+import PostController from '@/resources/post/post.controller';
 import App from './app'
 
 validateEnv();
 
-const app = new App([], Number(process.env.PORT));
+const app = new App([new PostController()], Number(process.env.PORT));
 
 app.listen();
---
+++ src/resources/post/post.model.ts
@@ -0,0 +1,16 @@
+import { Schema, model } from 'mongoose';
+import Post from '@/resources/post/post.interface';
+
+
+const PostSchema = new Schema({
+      title: {
+            type: String,
+            required: true
+      },
+      body: {
+            type: String,
+            required: true
+      }
+}, { timestamps: true });
+
+export default model<Post>('Posts', PostSchema)
